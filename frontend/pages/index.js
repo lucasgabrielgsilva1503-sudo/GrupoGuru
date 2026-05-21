dda09fa1ea0a6f34835c3c37ca6e8a1359bb7e4f
lucasgabrielgsilva1503-sudo <lucasgabrielgsilva1503@gmail.com>
2026-05-21 20:17:29 -0300

    Create index.js

---
+++ frontend/pages/index.js
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>GrupoGuru</h1>
+
+      <p>
+        Encontre grupos ativos de Facebook,
+        Telegram e Discord.
+      </p>
+
+      <Link href="/search">
+        <button>Pesquisar Grupos</button>
+      </Link>
+    </div>
+  );
+}
